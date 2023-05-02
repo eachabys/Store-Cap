89fe7d04efb2150750f649aa10e29e1b91c2420e
eachabys <44489101+eachabys@users.noreply.github.com>
2023-05-02 08:17:41 -0500

    Add files via upload

---
+++ trendsdemo/src/main/java/com/postgresql/trendsdemo/TrendsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.trendsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendsdemoApplication.class, args);
+	}
+
+}
