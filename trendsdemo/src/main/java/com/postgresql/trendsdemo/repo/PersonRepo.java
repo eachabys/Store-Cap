49b2535212f2d423f4bbc409e4ec2b9479755480
eachabys <44489101+eachabys@users.noreply.github.com>
2023-06-02 16:49:49 -0500

    Delete trendsdemo directory

--- trendsdemo/src/main/java/com/postgresql/trendsdemo/repo/PersonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.postgresql.trendsdemo.repo;
-
-import com.postgresql.trendsdemo.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-@RepositoryRestResource
-public interface PersonRepo extends JpaRepository<Person, Long>{
-}
