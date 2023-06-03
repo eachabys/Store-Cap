1066ca525f1515d3c8a9293e2ae2fe5fd2d9cd9c
eachabys <44489101+eachabys@users.noreply.github.com>
2023-06-03 13:08:29 -0500

    Add files via upload

---
+++ ordersApp/src/main/java/com/example/noteApp/repositories/NoteRepository.java
@@ -0,0 +1,14 @@
+package com.example.noteApp.repositories;
+
+import com.example.noteApp.entities.Notes;
+import com.example.noteApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Notes, Integer> {
+    List<Notes> findAllByUserEquals(User user);
+
+}
