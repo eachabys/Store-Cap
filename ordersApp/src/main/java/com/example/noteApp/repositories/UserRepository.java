e500b35b01cba88492afd6d33fc72db24038aea5
eachabys <44489101+eachabys@users.noreply.github.com>
2023-06-13 15:37:26 -0500

    Add files via upload

---
+++ ordersApp/src/main/java/com/example/noteApp/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.noteApp.repositories;
+
+import com.example.noteApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+}
